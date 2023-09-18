5b544c6cefc3326b96da3ae6b7e42b9d37896a22
RodolfoQuintanilla <rqm@outlook.es>
2023-09-18 13:38:09 -0600

    Fin

--- criptoxxx/src/App.jsx
+++ criptoxxx/src/App.jsx
@@ -1,12 +1,14 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled'
 
-import ImagenCR from "./assets/img/imagen-criptos.png";
-import Formulario from "./components/Formulario";
-import { useEffect, useState } from "react";
-import Resultado from "./components/Resultado";
+import ImagenCR from './assets/img/imagen-criptos.png'
+import Formulario from './components/Formulario'
+import { useEffect, useState } from 'react'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
+
 
 const Heading = styled.h1`
-  font-family: "Lato", sans-serif;
+  font-family: 'Lato', sans-serif;
   color: #fff;
   text-align: center;
   font-weight: 700;
@@ -15,14 +17,14 @@ const Heading = styled.h1`
   font-size: 34px;
 
   &::after {
-    content: "";
+    content: '';
     width: 100px;
     height: 6px;
     background-color: #66a2fe;
     display: block;
     margin: 10px auto 0 auto;
   }
-`;
+`
 const Contenedor = styled.div`
   max-width: 900px;
   margin: 0 auto;
@@ -32,34 +34,37 @@ const Contenedor = styled.div`
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
   }
-`;
+`
 
 const Imegen = styled.img`
   max-width: 400px;
   width: 88%;
   margin: 100px auto 0 auto;
   display: block;
-`;
+`
 
 function App() {
-  const [monedas, setMonedas] = useState({});
-  const [resultado, setResultado] = useState({});
+  const [monedas, setMonedas] = useState({})
+  const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if (Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
-        const { moneda, criptomonedas } = monedas;
-        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomonedas}&tsyms=${moneda}`;
+        setCargando(true)
+        const { moneda, criptomonedas } = monedas
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomonedas}&tsyms=${moneda}`
 
-        const respuesta = await fetch(url);
-        const resultado = await respuesta.json();
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
 
-        setResultado(resultado.DISPLAY[criptomonedas][moneda]);
-      };
+        setResultado(resultado.DISPLAY[criptomonedas][moneda])
+        setCargando(false)
+      }
 
-      cotizarCripto();
+      cotizarCripto()
     }
-  }, [monedas]);
+  }, [monedas])
 
   return (
     <Contenedor>
@@ -68,10 +73,12 @@ function App() {
         <Heading>Cotiza Criptomonedas al Instante</Heading>
         <Formulario setMonedas={setMonedas} />
 
+        {cargando && <Spinner/>}
+
         {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
-  );
+  )
 }
 
-export default App;
+export default App
